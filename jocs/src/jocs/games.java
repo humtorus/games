2f1bc8ac6d3d30d0e7943c1b062256328124e3ae
Bogdan <voyager090577@gmail.com>
2019-10-01 17:00:04 +0200

    jocs

---
+++ jocs/src/jocs/games.java
@@ -0,0 +1,25 @@
+package jocs;
+
+import java.util.Scanner;
+
+public class games {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder menu = new StringBuilder("1.Go\n2.Chess");
+		String opcio;
+		System.out.println(menu);
+		opcio = sc.nextLine();
+		switch(Integer.parseInt(opcio)) {
+		case 1:
+			System.out.println("Soc el Go");
+			break;
+		case 2:
+			System.out.println("Soc el Chess");
+			break;
+		default:break;
+		}
+	}
+
+}
